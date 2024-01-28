87d1c6344854eb7f0d869ea2497c951e57b0218d
Steven De Jesus <sdejesu1>
2024-01-28 17:46:50 -0500

    successfully added link

--- simulate.py
+++ simulate.py
@@ -3,9 +3,11 @@ import time as t
 
 physicsClient = p.connect(p.GUI)
 
+p.loadSDF("box.sdf")
+
 for i in range(1, 1001):
 	p.stepSimulation()
 	print(i)
-	t.sleep(1/60)
+	t.sleep(1/70)
 
 p.disconnect()
