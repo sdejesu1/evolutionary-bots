534fb81e2c7df487916885a945a98962de872aac
Steven De Jesus <sdejesu1>
2024-01-28 21:17:17 -0500

    successfully stacked decreasingtower

--- generate.py
+++ generate.py
@@ -11,6 +11,13 @@ width = 1
 x = 0
 y = 0
 z = 0.5
-pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[x, y, z] , size=[length, width, height])
+
+#pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
+#pyrosim.Send_Cube(name="Box2", pos=[x, y, z + 1] , size=[length, width, height])
+
+for i in range(0,10):
+	print("Box" + str(i+1))
+	pyrosim.Send_Cube(name="Box" + str(i+1), pos=[x, y, z + i] , size=[length * (.9 ** (i+1)), width *(.9 ** (i+1)), height * (.9 ** (i+1))])
+
+
 pyrosim.End()
