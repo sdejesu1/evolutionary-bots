bc3b162cfc82e306399289473acbda8a99689e53
Steven De Jesus <sdejesu1>
2024-01-28 21:54:45 -0500

    5x5 boxes success

--- generate.py
+++ generate.py
@@ -15,9 +15,13 @@ z = 0.5
 #pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
 #pyrosim.Send_Cube(name="Box2", pos=[x, y, z + 1] , size=[length, width, height])
 
-for i in range(0,10):
-	print("Box" + str(i+1))
-	pyrosim.Send_Cube(name="Box" + str(i+1), pos=[x, y, z + i] , size=[length * (.9 ** (i+1)), width *(.9 ** (i+1)), height * (.9 ** (i+1))])
+
+for idx_y in range(0, 5):
+	y = idx_y
+	for idx_x in range(0, 5):
+		x = idx_x
+		for idx_boxes in range(0,10):
+        		pyrosim.Send_Cube(name="Box" + str(idx_boxes+1), pos=[x, y, z + idx_boxes] , size=[length * (.9 ** (idx_boxes+1)), width *(.9 ** (idx_boxes+1)), height * (.9 ** (idx_boxes+1))])
 
 
 pyrosim.End()
