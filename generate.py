74e0cc1aaf0699e543fa7965c803b96d97d2673d
Steven De Jesus <sdejesu1>
2024-02-02 10:24:44 -0500

    created torso, moved world box

--- generate.py
+++ generate.py
@@ -12,7 +12,17 @@ z = 0.5
 
 def Create_World():
 	pyrosim.Start_SDF("world.sdf")
-	pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
+	pyrosim.Send_Cube(name="Box", pos=[x+2, y+2, z] , size=[length, width, height])
 	pyrosim.End()
 
+
+def Create_Robot():
+	pyrosim.Start_URDF("body.urdf")
+	pyrosim.Send_Cube(name="Torso", pos=[x, y, z] , size=[length, width, height])
+	pyrosim.End()
+
+
+
+
 Create_World()
+Create_Robot()
