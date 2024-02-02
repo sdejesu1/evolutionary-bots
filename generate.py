914a3c47323160f4e3b4fec8592ee5e53a0649d5
Steven De Jesus <sdejesu1>
2024-02-02 14:08:30 -0500

    created multi link & multi joint robot

--- generate.py
+++ generate.py
@@ -25,6 +25,26 @@ link2_x = 0
 link2_y = 0
 link2_z = 0.5
 
+# position of link3
+link3_x = 0
+link3_y = 0.5
+link3_z = 0
+
+# position of link4
+link4_x = 0
+link4_y = 0
+link4_z = 0
+
+# position of link5
+link5_x = 0
+link5_y = 0
+link5_z = -0.5
+
+# position of link6
+link6_x = 0
+link6_y = 0
+link6_z = -0.5
+
 def Create_World():
 	pyrosim.Start_SDF("world.sdf")
 	pyrosim.Send_Cube(name="Box", pos=[world_x, world_y, world_z] , size=[length, width, height])
@@ -38,6 +58,14 @@ def Create_Robot():
 	pyrosim.Send_Cube(name="Link1", pos=[link1_x, link1_y, link1_z] , size=[length, width, height])
 	pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0,0,1])
 	pyrosim.Send_Cube(name="Link2", pos=[link2_x, link2_y, link2_z] , size=[length, width, height])
+	pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0,0.5,0.5])
+	pyrosim.Send_Cube(name="Link3", pos=[link3_x, link3_y, link3_z] , size=[length, width, height])
+	pyrosim.Send_Joint( name = "Link3_Link4" , parent= "Link3" , child = "Link4" , type = "revolute", position = [0,1,0])
+	pyrosim.Send_Cube(name="Link4", pos=[link4_x, link4_y, link4_z] , size=[length, width, height])
+	pyrosim.Send_Joint( name = "Link4_Link5" , parent= "Link4" , child = "Link5" , type = "revolute", position = [0,0,-0.5])
+	pyrosim.Send_Cube(name="Link5", pos=[link5_x, link5_y, link5_z] , size=[length, width, height])
+	pyrosim.Send_Joint( name = "Link5_Link6" , parent= "Link5" , child = "Link6" , type = "revolute", position = [0,0,-1])
+	pyrosim.Send_Cube(name="Link6", pos=[link6_x, link6_y, link6_z] , size=[length, width, height])
 	pyrosim.End()
 
 
