6f9d3c36555ad60651441e1ee578089ac5039822
Steven De Jesus <sdejesu1>
2024-02-23 13:55:30 -0500

    created simulation and constants files

---
+++ constants.py
@@ -0,0 +1,15 @@
+import numpy as numpy
+
+# Joint Control Parameters
+BACK_AMPLITUDE = numpy.pi / 8
+BACK_FREQUENCY = 10
+BACK_PHASE_OFFSET = numpy.pi
+
+FRONT_AMPLITUDE = numpy.pi / 4
+FRONT_FREQUENCY = 10
+FRONT_PHASE_OFFSET = numpy.pi
+
+# Simulation Parameters
+NUM_SIMULATION_STEPS = 1000
+SIMULATION_TIME_STEP = 1 / 1000
+MAX_MOTOR_FORCE = 100
