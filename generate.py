374fe456855f976d37f2383be0b2b42b9520bac1
Steven De Jesus <sdejesu1>
2024-03-25 17:59:18 -0400

    completed randomsearch module

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 
 # constants
 # size of box
@@ -21,13 +22,16 @@ def Generate_Brain():
 
     pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
-
+    '''
     pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=1.0)
     pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=1.0)
     #pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=1.0)
     pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=12.0)
-
-
+    '''
+    # two nested loops: outside - sensor, inside - motor
+    for i in range(0, 3):
+        for j in range(3, 5):
+            pyrosim.Send_Synapse(sourceNeuronName=i, targetNeuronName=j, weight=random.randint(-1, 1))
 
     pyrosim.End()
 
